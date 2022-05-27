d2a492c9ad86ed9cf6c700c385746fd7c42c4103
helmi <helmizone20@gmail.com>
2022-05-27 21:15:19 +0700

    add soal 1

---
+++ soal1.js
@@ -0,0 +1,20 @@
+// palindrome adalah kata yang bisa dibalik dan akan menjadi kata tersebut lagi
+// contoh "racecar" dibalki "racecar"
+
+// soal: buat function yang menentukan kata tersebut adalah palindrome atau bukan
+
+
+function isPalindrome(string) {
+    // code here
+
+};
+
+console.log(isPalindrome('racecar')); // return true
+
+console.log(isPalindrome('laptop')) // return false
+
+console.log(isPalindrome('radar')) // return true
+
+console.log(isPalindrome('rotator')) // return true
+
+console.log(isPalindrome('dinda')) // return false
